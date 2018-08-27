25bb64d4970a3f061a438ac4d5be48786e4c757c
Maciej Nycz <maciejnycz@Maciejs-MacBook-Air.local>
2018-08-27 12:08:35 +0200

    Calculcator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import java.lang.*;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.addAToB(5, 5);
+
+        if (resultAdd == 10) {
+            System.out.println("Calculator OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int resultSubstract = calculator.substractAFromB(15, 5);
+
+        if (resultSubstract == 10) {
+            System.out.println("Calculator OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
