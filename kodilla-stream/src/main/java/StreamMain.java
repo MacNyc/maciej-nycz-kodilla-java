2a8557dc998106245ead4fc216b35c0fba6d04e8
Maciej Nycz <maciejnycz@Maciejs-MacBook-Air.local>
2018-09-18 11:42:18 +0100

    Test

--- kodilla-stream/src/main/java/StreamMain.java
+++ kodilla-stream/src/main/java/StreamMain.java
@@ -1,9 +1,11 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.reference.FuncionalDecorator;
 
-import static jdk.nashorn.internal.objects.NativeString.*;
+import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
+import static jdk.nashorn.internal.objects.NativeString.trim;
 
 
 public class StreamMain {
@@ -14,5 +16,9 @@ public class StreamMain {
         poemBeautifier.beautify( "Mockito is not coll", FuncionalDecorator::toUpper);
         poemBeautifier.beautify( "I NEED TO FIGURE SOMETHING OUT LIKE AN EXAMPLE", a -> toLowerCase(a));
         poemBeautifier.beautify( "         Java       ", a -> trim(a));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
+
 }
