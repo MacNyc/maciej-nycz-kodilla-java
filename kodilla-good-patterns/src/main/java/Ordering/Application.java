04832502cc0687d787264aed1c2b23b096a3bddb
Maciej Nycz <maciejnycz@Maciejs-MacBook-Air.local>
2018-10-19 15:10:31 +0100

    zlegle 9

--- kodilla-good-patterns/src/main/java/Ordering/Application.java
+++ kodilla-good-patterns/src/main/java/Ordering/Application.java
@@ -2,12 +2,13 @@ package Ordering;
 
 public class Application {
     public static void main(String[] args) {
-        OrderRequestRetriever orr = new OrderRequestRetriever();
-        OrderRequest request = orr.retrieve();
+        User user = new User(2,"MMNycz","Maciej","Nycz", "NN95TL", 8, "nycz.maciej91@gmail.com", "07405479001");
+        Product product = new Product(3, "MacBook", 750.0);
+        int[] quntity = new int[3];
 
-        OrderProcessor processor = new OrderProcessor(new SMSService(), new SimpleOrderService(), new InMemoryOrderRepository());
-        OrderDto result = processor.process(request);
+        ProductOrderService productOrderService = new ProductOrderService(user,product,quntity);
 
-        System.out.println("Created order: " + result.getOrder());
+
+        System.out.println("Created order: " + productOrderService);
     }
 }
--- kodilla-good-patterns/src/main/java/Ordering/ProductOrderService.java
+++ kodilla-good-patterns/src/main/java/Ordering/ProductOrderService.java
@@ -1,6 +1,9 @@
 package Ordering;
 
 public class ProductOrderService {
+    public ProductOrderService(User user, Product product, int[] quntity) {
+    }
+
     public boolean order(final User user, final Product product, final int quantity){
         System.out.println("Creating orders for" + user.getNickName() + "\nItem: " + product.getName() +
                 "\nQuantity: "+ quantity);
