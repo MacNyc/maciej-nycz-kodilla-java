19ca69ff1da30e51dfd0acf95ab052d0161eca01
Maciej Nycz <nycz.maciej91@gmail.com>
2019-01-19 12:19:43 +0000

    23.4

---
+++ kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
@@ -0,0 +1,109 @@
+package com.kodilla.testing2.crudapp;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.Random;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertNotEquals;
+
+public class CrudAppTestSuite {
+    private static final String BASE_URL = "http://macnyc.github.io/";
+    private WebDriver driver;
+    private Random generator;
+
+    @Before
+    public void initTest() {
+        driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get(BASE_URL);
+        generator = new Random();
+    }
+
+    @After
+    public void cleanUpAfterTest(){
+        driver.close();
+    }
+
+
+    private String createCrudAppTestTask() throws InterruptedException {
+        final String XPATH_TASK_NAME = "//form[contains(@action, \"createTask\")]/fieldset[1]/input";
+        final String XPATH_TASK_CONTENT = "//form[contains(@action, \"createTask\")]/fieldset[2]/textarea";
+        final String XPATH_ADD_BUTTON = "/html/body/main/section[1]/form/fieldset[3]/button";
+        String taskName = "Task number " + generator.nextInt(100000);
+        String taskContent = taskName + " content";
+
+        WebElement name = driver.findElement(By.xpath(XPATH_TASK_NAME));
+        name.sendKeys(taskName);
+
+        WebElement content = driver.findElement(By.xpath(XPATH_TASK_CONTENT));
+        content.sendKeys(taskContent);
+
+        WebElement addButton = driver.findElement(By.xpath(XPATH_ADD_BUTTON));
+        addButton.click();
+        Thread.sleep(2000);
+
+        return taskName;
+    }
+    private void sendTestTaskToTrello(String taskName) throws InterruptedException{
+        driver.navigate().refresh();
+
+        while (!driver.findElement(By.xpath("//select[1]")).isDisplayed());
+
+        driver.findElements(By.xpath("//form[@class=\"datatable_row\"]")).stream()
+                .filter(anyForm ->
+                        anyForm.findElement(By.xpath(".//p[@class-\"datatable_field-value\"]"))
+                            .getText().equals(taskName))
+                .forEach(theForm -> {
+                    WebElement selectElement = theForm.findElement(By.xpath(".//select[1]"));
+                    Select select = new Select(selectElement);
+                    select.selectByIndex(1);
+
+                    WebElement buttonCreateCard =
+                            theForm.findElement(By.xpath("./button[contains(@class, \"card-creation\")]"));
+                    buttonCreateCard.click();
+                });
+        Thread.sleep(5000);
+
+    }
+
+    private void deleteCrudAppTestTask (String taskName){
+        WebDriverWait wait = new WebDriverWait(driver, 2, 100);
+
+        By locator = By.xpath("//form[@class=\"datatable__row\"]");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+        List<WebElement> taskRows = driver.findElements(locator).stream()
+                .filter(anyForm -> anyForm.findElement(By.xpath(".//p[@class=\"datatable__field-value\"]"))
+                        .getText().equals(taskName)).collect(toList());
+
+        assertNotEquals(0, taskRows.size());
+
+        for (WebElement element : taskRows) {
+            wait.until(ExpectedConditions.visibilityOf(element));
+        }
+
+        for (WebElement taskRow : taskRows) {
+            WebElement button = taskRow.findElement(By.xpath(".//button[@data-task-delete-button=\"\"]"));
+            wait.until(ExpectedConditions.elementToBeClickable(button)).click();
+            wait.until(ExpectedConditions.stalenessOf(taskRow));
+        }
+    }
+
+    @Test
+    public void shouldCreateTrelloCard() throws InterruptedException {
+        String taskName = createCrudAppTestTask();
+        sendTestTaskToTrello(taskName);
+        deleteCrudAppTestTask(taskName);
+    }
+
+}
